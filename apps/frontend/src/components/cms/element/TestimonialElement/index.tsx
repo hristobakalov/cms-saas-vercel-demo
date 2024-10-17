f8977c4a0647f82cd85380a8cbbdc3ac928ea569
Hristo B <hristobak@gmail.com>
2024-10-17 09:08:29 +0200

    fix testi

--- apps/frontend/src/components/cms/element/TestimonialElement/index.tsx
+++ apps/frontend/src/components/cms/element/TestimonialElement/index.tsx
@@ -30,19 +30,11 @@ export const TestimonialElementElement: CmsComponent<
         className="rich-text m-0 p-0 border-l-0 prose-p:mt-0"
       />
       <figcaption>
-        <cite className="ml-4 lg:flex text-white lg:justify-start lg:items-center not-italic">
+        <cite className="lg:flex text-white lg:justify-start lg:items-center not-italic">
           <p className="whitespace-nowrap align-middle mt-0 mb-2 lg:mb-0">
-            {customerImage && (
-              <CmsImage
-                src={customerImage}
-                alt={customerName ?? ""}
-                width={200}
-                height={200}
-                className="rounded-full h-12 w-12 mr-3 not-prose inline-block"
-              />
-            )}
             {customerName ?? ""}
           </p>
+
           {customerLocation && (
             <>
               <span className="mx-2 hidden lg:inline-block">—</span>
@@ -52,6 +44,17 @@ export const TestimonialElementElement: CmsComponent<
             </>
           )}
         </cite>
+        <div>
+          {customerImage && (
+            <CmsImage
+              src={customerImage}
+              alt={customerName ?? ""}
+              width={200}
+              height={200}
+              className="rounded-full h-24 w-40 mr-3 not-prose object-contain inline-block"
+            />
+          )}
+        </div>
       </figcaption>
     </CmsEditable>
   );
